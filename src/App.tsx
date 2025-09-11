c5530db9dfd6552e8fccf9006925d5d95845f116
Vitaly Ibragimov <leazaz322@gmail.ru>
2025-09-11 12:08:39 +0500

    Rew with Timur

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import {Route, Routes} from "react-router-dom";
 import Profile from "./pages/Profile/Profile";
 import MainLayouts from "@/layouts/MainLayouts/MainLayouts";
 import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
+import AuthLayouts from "@/layouts/AuthLayouts/AuthLayouts";
+import Authorization from "@/pages/Authorization/Authorization";
+import Registration from "@/pages/Registration/Registration";
 
 function App(): React.JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<MainLayouts/>}>
-        <Route path="/" element={<TodoListPage />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route index element={<TodoListPage />} />
+        <Route path="profile" element={<Profile />} />
         <Route path="*" element={<NotFoundPage/>} />
       </Route>
+{/*      <Route path="/authorization" element={<AuthLayouts/>}>
+        <Route index element={<Authorization/>}/>
+        <Route path="registration" element={<Registration/>}/>
+      </Route>*/}
+
     </Routes>
   )
 }
---
+++ src/pages/Authorization/Authorization.tsx
@@ -0,0 +1,11 @@
+import AuthMenu from "@/components/AuthMenu/AuthMenu";
+
+const Authorization = () => {
+  return (
+    <div>
+      <AuthMenu/>
+    </div>
+  );
+};
+
+export default Authorization;
